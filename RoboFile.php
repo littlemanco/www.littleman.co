1faa3e03cf4200ffa9710aabaa3df6e5dee309b5
Andrew Howden <hello@andrewhowden.com>
2019-05-02 18:50:37 +0200

    Add build environment container

--- RoboFile.php
+++ RoboFile.php
@@ -6,16 +6,54 @@
  */
 class RoboFile extends \Robo\Tasks
 {
+    const CONTAINER_PATH = 'build/containers';
+    const CONTAINER_NAMESPACE = 'gcr.io/littleman-co/www-littleman-co';
+
     /**
      * Runs lints over the codebase
-     * 
+     *
      * @option files A space separated list of files to lint
      */
     public function lint()
-    {   
+    {
         $this->taskExecStack()
             ->stopOnFail()
             ->exec('yamllint .')
             ->run();
+    }
+
+    /**
+     * Builds containers. Available containers are those at the path "build/containers"
+     *
+     * @option container The container to build
+     */
+    public function containerBuild($opts = ['container' => 'web'])
+    {
+        $refspec       = exec('git rev-parse --short HEAD');
+        $containerName = self::CONTAINER_NAMESPACE . '--' . $opts['container'];
+
+        $this->taskDockerBuild(self::CONTAINER_PATH . DIRECTORY_SEPARATOR . $opts['container'])
+            ->tag($containerName . ':' . $refspec)
+            ->tag($containerName . ':latest' )
+            ->run();
+    }
+
+    /**
+     * Pushes containers
+     * 
+     * @option container The container to push upstream
+     */
+    public function containerPush($opts = ['container' => 'web'])
+    {
+        $refspec       = exec('git rev-parse --short HEAD');
+        $containerName = self::CONTAINER_NAMESPACE . '--' . $opts['container'];
+
+        $this->taskExec('docker')
+            ->args(['push', $containerName . ':' . $refspec])
+            ->run();
+        $this->taskExec('docker')
+            ->args(['push', $containerName . ':latest'])
+            ->run();
+           
     }
 }
